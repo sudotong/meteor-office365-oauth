ac70c9b547965754d87c000079c510d271ce0117
Sam Udotong <sudotong@gmail.com>
2019-02-10 20:41:55 -0800

    fix office oauth email pulling

--- office365_server.js
+++ office365_server.js
@@ -1,92 +1,138 @@
 /*globals Office365, OAuth */
-'use strict';
+"use strict";
 
 Office365 = {};
 
-let userAgent = 'Meteor';
-if (Meteor.release) { userAgent += `/${ Meteor.release }`; }
+let userAgent = "Meteor";
+if (Meteor.release) {
+  userAgent += `/${Meteor.release}`;
+}
 
-const getTokens = function(query) {
-  const config = ServiceConfiguration.configurations.findOne({service: 'office365'});
-  if (!config) { throw new ServiceConfiguration.ConfigError(); }
-
-  const redirectUri = OAuth._redirectUri('office365', config).replace('?close', '');
-
-  const scope = config.permissions || ['offline_access', 'user.read'];
-  const flatScope = _.map(scope, encodeURIComponent).join('%20');
-
-  // https://github.com/microsoftgraph/microsoft-graph-docs/blob/master/concepts/auth_v2_user.md
-
+const getAccessFromRefresh = function (refreshToken) {
   let response;
-  try {
-    response = HTTP.post(
-      `https://login.microsoftonline.com/${ config.tenant || 'common' }/oauth2/v2.0/token`, {
-        headers: {
-          Accept: 'application/json',
-          'User-Agent': userAgent
-        },
-        params: {
-          scope: scope.join(' '),
-          grant_type: 'authorization_code',
-          code: query.code,
-          client_id: config.clientId,
-          client_secret: OAuth.openSecret(config.secret),
-          redirect_uri: redirectUri,
-          state: query.state
-        }
-      });
-  } catch (error) {
-    throw _.extend(new Error(`Failed to complete OAuth handshake with Microsoft Office365. ${ error.message }`), {response: error.response});
-  }
-  if (response.data.error) {
-    throw new Error(`Failed to complete OAuth handshake with Microsoft Office365. ${ response.data.error }`);
+  const config = ServiceConfiguration.configurations.findOne({ service: "office365" });
+  if (!config) throw new ServiceConfiguration.ConfigError();
+  let params = {
+    scope: `offline_access openid profile User.Read Calendars.Read Calendars.ReadWrite`,
+    client_id: config.clientId,
+    client_secret: OAuth.openSecret(config.secret),
+    redirect_uri: OAuth._redirectUri("office365", config).replace("?close", "")
+  };
+  params.grant_type = "refresh_token";
+  params.refresh_token = refreshToken;
+
+  response = HTTP.post(`https://login.microsoftonline.com/${config.tenant || "common"}/oauth2/v2.0/token`, {
+    headers: { Accept: "application/json", "User-Agent": userAgent }, params
+  }).data;
+
+  return response.access_token;
+}
+
+const getTokens = function (query) {
+  let response;
+  const config = ServiceConfiguration.configurations.findOne({ service: "office365" });
+  if (!config) throw new ServiceConfiguration.ConfigError();
+
+  let params = {
+    scope: `offline_access openid profile User.Read Calendars.Read Calendars.ReadWrite`,
+    code: query.code, client_id: config.clientId,
+    client_secret: OAuth.openSecret(config.secret),
+    redirect_uri: OAuth._redirectUri("office365", config).replace("?close", ""),
+    state: query.state
+  };
+
+  if (Boolean(config.refresh_token)) {
+    params.grant_type = "refresh_token";
+    params.refresh_token = config.refresh_token;
+    ServiceConfiguration.configurations.upsert({ service: "office365" }, { $set: { refresh_token: '' } });
   } else {
-    return response.data;
+    params.grant_type = "authorization_code";
   }
+  response = HTTP.post(`https://login.microsoftonline.com/${config.tenant || "common"}/oauth2/v2.0/token`, { headers: { Accept: "application/json", "User-Agent": userAgent }, params }).data;
+
+  if (response && params.grant_type === "authorization_code") {
+    ServiceConfiguration.configurations.upsert({ service: "office365" }, { $set: { refresh_token: response.refresh_token } });
+  }
+
+  return response;
 };
 
-const getIdentity = function(accessToken) {
+const getIdentity = function (accessToken) {
   try {
-    return HTTP.get(
-      'https://outlook.office.com/api/v2.0/me', {
-        headers: {
-          Authorization: `Bearer ${ accessToken }`,
-          'Content-Type': 'application/json',
-          'User-Agent': userAgent
-        }
-      }).data;
+    return HTTP.get("https://graph.microsoft.com/v1.0/me", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+        "User-Agent": userAgent
+      }
+    }).data;
   } catch (error) {
-    throw _.extend(new Error(`Failed to fetch identity from Microsoft Office365. ${ error.message }`), {response: error.response});
+    console.log(error.message)
   }
 };
 
-OAuth.registerService('office365', 2, null, function(query) {
-  const data = getTokens(query);
-  const identity = getIdentity(data.access_token);
-  if (!identity.userPrincipalName) identity.userPrincipalName = identity.EmailAddress;
-  return {
-    serviceData: {
+Meteor.methods({
+  getEmail: function (refresh_token, access_token) {
+    console.log("this is being called")
+    if (access_token) {
+      let obj = getIdentity(access_token);
+      return obj.mail || obj.userPrincipalName
+    } else {
+      let obj = getIdentity(getAccessFromRefresh(refresh_token));
+      return obj.mail || obj.userPrincipalName
+    }
+  }
+})
+
+OAuth.registerService("office365", 2, null, function (query, other) {
+  let data;
+
+  try {
+    data = getTokens(query);
+  } catch (error) {
+    console.log(error.message)
+  }
+
+  if (data) {
+    const identity = getIdentity(data.access_token);
+
+    if (!identity.userPrincipalName) identity.userPrincipalName = identity.EmailAddress;
+
+    const serviceData = {
       id: identity.id || identity.Id,
       accessToken: data.access_token,
       refreshToken: data.refresh_token,
-      expiresAt: data.expires_in ? data.expires_in*1000 + new Date().getTime() : null,
+      expiresAt: data.expires_in
+        ? data.expires_in * 1000 + new Date().getTime()
+        : null,
       scope: data.scope,
-      displayName: identity.displayName || identity.DisplayName || identity.Alias,
-      givenName: identity.givenName || identity.Alias,
+      displayName: identity.displayName ||
+        identity.DisplayName ||
+        identity.Alias,
+      givenName: identity.givenName || identity.Alias || identity.displayName,
       surname: identity.surname,
-      username: identity.userPrincipalName && identity.userPrincipalName.split('@')[0],
+      username: identity.userPrincipalName &&
+        identity.userPrincipalName.split("@")[0],
       userPrincipalName: identity.userPrincipalName,
-      mail: identity.mail,
+      mail: identity.mail ? identity.mail : identity.userPrincipalName,
       jobTitle: identity.jobTitle,
       mobilePhone: identity.mobilePhone,
       businessPhones: identity.businessPhones,
       officeLocation: identity.officeLocation,
       preferredLanguage: identity.preferredLanguage
-    },
-    options: {profile: {name: identity.givenName}}
-  };
+    }
+    return {
+      serviceData,
+      options: { profile: { name: identity.givenName } }
+    };
+  } else {
+    return {
+      serviceData: {}
+    }
+  }
+
 });
 
-Office365.retrieveCredential = function(credentialToken, credentialSecret) {
+Office365.retrieveCredential = function (credentialToken, credentialSecret) {
   return OAuth.retrieveCredential(credentialToken, credentialSecret);
 };
