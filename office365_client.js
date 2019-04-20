84da83047801b584a30d1cdfc47798d045576027
Sam Udotong <sudotong@gmail.com>
2019-04-20 14:14:01 -0700

    Add redirect url for office

--- office365_client.js
+++ office365_client.js
@@ -1,22 +1,30 @@
 /*globals Office365, OAuth */
+import { Tracker } from 'meteor/tracker';
+import { Session } from 'meteor/session';
 
 Office365 = {};
 
 Office365.requestCredential = function(options, credentialRequestCompleteCallback) {
+
+  Session.set("redirectUrl", options.redirectUrl );
+
   if (!credentialRequestCompleteCallback && typeof options === 'function') {
     credentialRequestCompleteCallback = options;
     options = {};
   }
 
   const config = ServiceConfiguration.configurations.findOne({service: 'office365'});
+
   if (!config) {
     credentialRequestCompleteCallback && credentialRequestCompleteCallback(new ServiceConfiguration.ConfigError());
     return;
   }
 
   const credentialToken = Random.secret();
+
   if (!options) options = {};
   if (!options.requestPermissions) options.requestPermissions = config.permissions;
+
   const scope = options.requestPermissions || ['offline_access', 'user.read'];
   const flatScope = _.map(scope, encodeURIComponent).join('%20');
 
@@ -26,6 +34,7 @@ Office365.requestCredential = function(options, credentialRequestCompleteCallbac
   const redirectUri = `${Meteor.absoluteUrl()}api/office365-auth`; //OAuth._redirectUri('office365', config).replace('?close', '');
 
   const loginUrl = `https://login.microsoftonline.com/${ config.tenant || 'common' }/oauth2/v2.0/authorize?client_id=${ config.clientId }&response_type=code&redirect_uri=${ redirectUri }&response_mode=query&scope=${ flatScope }&state=${ OAuth._stateParam(loginStyle, credentialToken, redirectUri) }`;
+
   OAuth.launchLogin({
     loginService: 'office365',
     loginStyle,
